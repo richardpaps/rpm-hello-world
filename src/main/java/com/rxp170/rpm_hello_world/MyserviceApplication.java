bd2ec1d538ba8faffe061fdc6242dcf0f1c39a77
rxp170 <rxp170@AN00619194.eis.equifax.com>
2017-01-25 13:23:41 -0500

    name changes2

--- src/main/java/com/rxp170/rpm_hello_world/MyserviceApplication.java
+++ src/main/java/com/rxp170/rpm_hello_world/MyserviceApplication.java
@@ -1,5 +1,13 @@
 package com.rxp170.rpm_hello_world;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MyserviceApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(MyserviceApplication.class, args);
+    }
 }
